2bdb99019cd24470cb05c4c774ca2cb01d72cb24
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-03 19:35:59 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by gasta on 03/05/2022.
+//
 
+#ifndef VALORE_MASSIMO_ARRAY_LIB_H
+#define VALORE_MASSIMO_ARRAY_LIB_H
+
+#endif //VALORE_MASSIMO_ARRAY_LIB_H
+
+float max_array (float, int);
