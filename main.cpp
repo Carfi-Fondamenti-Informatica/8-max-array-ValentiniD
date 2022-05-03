b1d7138405fd9b692ec7561dab009dcf63bbb3fe
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-03 19:37:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    int dimensione=0;
+    cin >> dimensione;
+    float array[dimensione];
+    for (int i=0;i<dimensione;i++) {
+        cin >> array[i];
+    }
+    float risultato=max_array(array,dimensione);
+    cout << risultato;
+
+    return 0;
 }
