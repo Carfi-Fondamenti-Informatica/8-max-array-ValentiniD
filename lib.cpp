c6a29ee2ae54689d809d84043863a839fccd2fea
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-03 19:36:36 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by gasta on 03/05/2022.
+//
 
+#include "lib.h"
+
+float max_array (float array[], int dimensione) {
+    float valore_massimo=0;
+    for (int i=0; i<dimensione; i++) {
+        if (valore_massimo<array[i]) {
+            valore_massimo=array[i];
+        }
+    }
+    return valore_massimo;
+}
